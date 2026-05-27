b8becd4d58ff36d09b8026429373db5b8433113c
daothikhuyen <daothikhuyen30@gmail.com>
2026-05-27 11:58:45 +0700

    feat(search) Delete delay when you load more

--- app/src/main/java/com/example/banknoteindentifier/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/banknoteindentifier/ui/search/SearchViewModel.kt
@@ -81,7 +81,7 @@ class SearchViewModel(private val bankNoteRepository: BankNoteRepository) : View
     private suspend fun updateList(list: List<BankNote>) {
         val currentList = _searchBankNotes.value.toMutableList()
         currentList.addAll(list)
-        delay(1000)
+//        delay(1000)
         _searchBankNotes.value = currentList
         if (list.isEmpty()) {
             isLastPage = true
